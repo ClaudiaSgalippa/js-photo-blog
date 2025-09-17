bacb6a173264897522ea5a55d9d33bc29932c944
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-17 18:00:09 +0200

    Completamento esercizio (manca l'hover)

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-//Ho usato Postman e ho notato la presenza di un Array con all'interno gli oggetti da inserire ("title", "date" e "url") nella card
+//-- GENERIAMO LA COSTANTE CHE COLLEGA IL JS ALL'HTML --//
+const containerCards = document.getElementById("container-main");
+
+//-- GENERIAMO UNA CHIAMATA AJAX ALL'API INDICATO --//
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
+
+    .then((responseCard) => {
+        const cards = responseCard.data; 
+
+        let fakePolaroid = ""; //Variabile vuota per l'inserimento della card completa
+
+        cards.forEach(card => { //Cycle per la generazione di tutte le cards
+
+            fakePolaroid += `<div class="card"> 
+                        <img src="img/pin.svg" alt="pin" class="pin">
+                        <div class="img">
+                            <img src="${card.url}" alt="photo">
+                        </div>
+                        <div>
+                            <h4>${card.title}</h4>
+                            <span>${card.date}</span>
+                        </div>
+                    </div>`
+        }) //Recuperiamo il <div> dall'HTML e inseriamo i collegamenti per gli oggetti 
+        containerCards.innerHTML = fakePolaroid;
+    })
